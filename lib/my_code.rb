14ea540389c5706c12c1c79269b22926fd433e58
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-12-08 22:25:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -64,8 +64,9 @@ end
 def reduce_to_any_true(source_array)
   i = 0 
   while i < source_array.length do
-    return true if source_array[i]
-    i += 1 
+    if source_array 
+      i += 1
+      return true 
   end
   return false
 end 
