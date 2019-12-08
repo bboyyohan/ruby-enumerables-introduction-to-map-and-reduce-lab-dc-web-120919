ca7515f0255d40264dbbab64260c376f4e847e99
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-12-08 21:51:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,13 @@ def map_to_negativize(source_array)
   end 
   return array
 end 
+
+def map_to_no_change(source_array)
+  array = []
+  i = 0 
+  while i < source_array.length do
+    array.push(source_array[i])
+    i += 1 
+  end
+  return array
+end 
